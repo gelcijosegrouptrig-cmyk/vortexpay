aec9df1d4b79c81d6ec251ba32dc2c6ea34cef1f
gelcijosegrouptrig-cmyk <genspark_dev@genspark.ai>
2026-04-13 15:10:21 +0000

    start.sh: COMMIT_HASH final = a3dc119f7ba798828f9ddb9049b26524891d4c77

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-COMMIT_HASH=d9410bac81a1646a219bce94ec716393ae46220e
+COMMIT_HASH=28d23a90cb2bc6f40051b6c5f53dfe455214696d
 REPO_RAW="https://raw.githubusercontent.com/gelcijosegrouptrig-cmyk/vortexpay/${COMMIT_HASH}"
 echo "=== VortexPay Deploy - UI-v30 ==="
 echo "Commit: $COMMIT_HASH"
